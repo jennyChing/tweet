739c17c2d4628556b23b1bd2401bd6c4ef546c22
jennyChing <jklife3@gmail.com>
2017-07-07 13:21:17 -0400

    Integrate twitter stream API to get post data

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
-var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var twit = require('twitter'),
+	client = new twit({
+		consumer_key:'lQCnX0y6UlDruC38ShhJhbfU5',
+		consumer_secret:'5Rs2dY8fz1RTPNOnxxWbMFAq7bh0TZyG2qrGLF6pHuit091vT4',
+		access_token_key:'2354050566-b021r62sNCJBgHCRiX7Co8cAZHmlgeO5BvOuSgs',
+		access_token_secret:'4xfA8BrAzEhRLZn6Uw9r0s0Kqu2FAeQ7194yNjvFNbiXz'
+	});
 
 var app = express();
 
@@ -9,15 +15,6 @@ app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
 
-var pool = mysql.createPool({
-	host: 'task1db.c0wykvs8ujyc.us-east-1.rds.amazonaws.com',
-    user: 'jklife3',
-    password: 'J23226006',
-    database: 'tweet',
-    port: '3306',
-    debug: true
-});
-
 app.get('/', function(req, res) {
 	res.render('index.jade', { title: 'index' , message: 'Input the username'});
 	// res.json({ message: 'Welcome to tweet!'});
@@ -25,25 +22,24 @@ app.get('/', function(req, res) {
 
 app.post('/showUserTweets', function(req, res) {
 	var userName = req.body.userName;
-	var userId;
-	pool.getConnection(function(err, connection) {
-	    connection.query("SELECT userId FROM userTable WHERE userName = ?", [userName], function(error, rows, fields) {
-			if(!!error || rows.length != 1) {
-				console.log(error);
-				res.render('index.jade', { message: 'There seems to be an issue with the username that you entered'});
-			} else {
-				userId = rows[0].userId;
-				connection.query("SELECT *, likes + replies + shares as totalScore FROM tweetTable WHERE userId = ? ORDER BY totalScore DESC LIMIT 10", [userId], function(error, rows, fields) {
-					if(!!error || rows.length < 1) {
-						console.log(error);
-						res.render('index.jade', { message: 'This user has no tweets so far.'});
-					} else {
-						res.render('index.jade', { message: 'Tweets for' + userName, data: rows});
-					}
-				});
+	var params = {screen_name: userName}; // count:
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			var data = [];
+			var score = 
+			console.log(tweets);
+			for (var i = 0; i < tweets.length ; i++) {
+				console.log(i + tweets[i].retweeted);
+				if (tweets[i].text.startsWith('RT @') == false){
+					score = tweets[i].retweet_count + tweets[i].favorite_count;
+					data.push({text: tweets[i].text, favorite_count: tweets[i].favorite_count, retweet_count: tweets[i].retweet_count, score: score, created_at: tweets[i].created_at});
+				};
 			}
-		});
-		connection.release();
+			data.sort(function(a, b){ 
+				return b.score - a.score;
+			});
+			res.render('index.jade', { message: 'Top ten tweets for ' + userName, data: data.slice(0, 10)});
+		}
 	});
 })
 
